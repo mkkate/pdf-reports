84087add7b85609b958f9d92874e73440c8fc026
Katarina Mladenovic <katarina.mladenovic.kate@gmail.com>
2025-01-04 00:36:28 +0100

    delete commented code

--- PdfReports.Web/Models/User.cs
+++ PdfReports.Web/Models/User.cs
@@ -6,13 +6,6 @@ namespace PdfReports.Web.Models
 {
     public class User
     {
-        //public List<User> Users { get; set; }
-
-        //public User()
-        //{
-
-        //}
-
         public int Id { get; set; }
 
         [DisplayName(DisplayNameConstants.FirstName)]
@@ -35,36 +28,5 @@ namespace PdfReports.Web.Models
 
         [DisplayName(DisplayNameConstants.Position)]
         public Position Position { get; set; } = new Position();
-
-        //private void CreateUsers(int count)
-        //{
-        //    Users = new List<User>();
-        //    for (int i = 0; i < count; i++)
-        //    {
-        //        var faker = new Faker();
-
-        //        Users.Add(new User
-        //        {
-        //            Id = i + 1,
-        //            FirstName = faker.Name.FirstName(),
-        //            LastName = faker.Name.LastName(),
-        //            Address = faker.Address.FullAddress(),
-        //            EmploymentStartDate = faker.Date.PastDateOnly(10),
-        //            EmploymentEndDate = faker.Random.Bool() ? faker.Date.FutureDateOnly(5) : null,
-        //            DaysOff = new DaysOff
-        //            {
-        //                Vacation = faker.Random.Number(0, 20),
-        //                Paid = faker.Random.Number(0, 10),
-        //                Unpaid = faker.Random.Number(0, 10),
-        //                SickLeave = faker.Random.Number(0, 10)
-        //            },
-        //            Position = new Position
-        //            {
-        //                Title = faker.Name.JobTitle(),
-        //                SeniorityLevel = faker.PickRandom(new[] { "Junior", "Medior", "Senior" })
-        //            }
-        //        });
-        //    }
-        //}
     }
 }
--- PdfReports.Web/Services/UserService.cs
+++ PdfReports.Web/Services/UserService.cs
@@ -12,38 +12,7 @@ namespace PdfReports.Web.Services
             for (int i = 0; i < count; i++)
             {
                 var faker = new Faker();
-
-                //var Id = i + 1;
-                //var FirstName = faker.Name.FirstName();
-                //var LastName = faker.Name.LastName();
-                //var Address = faker.Address.FullAddress();
-                //var EmploymentStartDate = faker.Date.PastDateOnly(10);
-                //DateOnly? EmploymentEndDate = faker.Random.Bool() ? faker.Date.FutureDateOnly(5) : null;
-                //var DaysOff = new DaysOff
-                //{
-                //    Vacation = faker.Random.Number(0, 22),
-                //    Paid = faker.Random.Number(0, 10),
-                //    Unpaid = faker.Random.Number(0, 10),
-                //    SickLeave = faker.Random.Number(0, 10)
-                //};
-                //var Position = new Position
-                //{
-                //    Title = faker.Name.JobTitle(),
-                //    SeniorityLevel = faker.PickRandom(new[] { "Junior", "Medior", "Senior" })
-                //};
-
-                //Users.Add(new User
-                //{
-                //    Id = Id,
-                //    FirstName = FirstName,
-                //    LastName = LastName,
-                //    Address = Address,
-                //    EmploymentStartDate = EmploymentStartDate,
-                //    EmploymentEndDate = EmploymentEndDate,
-                //    DaysOff = DaysOff,
-                //    Position = Position
-                //});
-
+                
                 Users.Add(new User
                 {
                     Id = i + 1,
@@ -73,11 +42,6 @@ namespace PdfReports.Web.Services
         {
             return Users;
         }
-
-        //public List<User> Create()
-        //{
-        //    return GenerateUsers(1);
-        //}
 
         public User Create(User userModel)
         {
